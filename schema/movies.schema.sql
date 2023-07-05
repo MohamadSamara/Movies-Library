6fb7e1896cc190b7a9dbc0ca052316debd2eccce
MohamadSamara <mohamadsamara1211@gmail.com>
2023-07-05 16:21:37 +0000

    for lab 15

--- schema/movies.schema.sql
+++ schema/movies.schema.sql
@@ -1,7 +1,7 @@
 create table if not exists movie(
     id serial primary key,
     title varchar(200),
-    release_dat varchar(200),
+    release_date varchar(200),
     poster_path varchar(200),
     overview varchar(1000)
 );
